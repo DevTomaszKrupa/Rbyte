85bb8ec2e033e8981cc1432c696a24361fe73a21
dev_TK <tomaszkrupa95@gmail.com>
2018-10-27 19:09:53 +0200

    Added var and out parameters example

--- App/Program.cs
+++ App/Program.cs
@@ -8,20 +8,32 @@ namespace Rbyte.App
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is my first .net core app!");
-            
+
             Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            
+            var name = Console.ReadLine();
+
             Console.WriteLine("How old are you?");
-            string ageStr = Console.ReadLine();
+            int age;
+            var intParsingResult = int.TryParse(Console.ReadLine(), out age); // this is bool value
+            if (!intParsingResult)
+            {
+                Console.WriteLine("You've entered the wrong value");
+                Console.WriteLine("Program ended.");
+                Console.ReadLine();
+                return;
+            }
+            var youAreAdult = age >= 18;
 
             Console.WriteLine("How tall are you? [m]");
-            string heightStr = Console.ReadLine();
-
-            decimal height = decimal.Parse(heightStr);
-
-            int age = int.Parse(ageStr);
-            bool youAreAdult = age >= 18;
+            decimal height;
+            var decimalParsingResult = decimal.TryParse(Console.ReadLine(), out height);
+            if (!decimalParsingResult)
+            {
+                Console.WriteLine("You've entered the wrong value");
+                Console.WriteLine("Program ended.");
+                Console.ReadLine();
+                return;
+            }
 
             Console.WriteLine($"It's nice to meet you, {name}");
             Console.WriteLine($"You are {age} old. You will be {age + 10} in ten years");
