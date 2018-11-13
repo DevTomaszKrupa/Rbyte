dd407e75b2eac73bf69375e5955649db1900ef9f
dev_TK <tomaszkrupa95@gmail.com>
2018-11-13 23:32:43 +0100

    Added service

---
+++ Rbyte/Src/Core/Rbyte.Application/Product/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using Rbyte.Application.Product.Read;
+using Rbyte.Domain.Entities;
+using Rbyte.Persistance;
+using Rbyte.Persistance.Infrastructure;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rbyte.Application.Product.Create
+{
+    public class ProductService
+    {
+        private readonly RbyteContext _context;
+
+        public ProductService()
+        {
+            _context = new DesignTimeDbContextFactory().CreateDbContext(null);
+        }
+
+        public void Create(CreateProductModel model)
+        {
+            _context.Products.Add(new DbProduct
+            {
+                Barcode = model.Barcode,
+                Description = model.Description,
+                Name = model.Name,
+                ProducerId = model.ProducerId,
+                StandardPrice = model.Price
+            });
+            _context.SaveChanges();
+        }
+
+        public ReadProductModel Read(int productId)
+        {
+            var product = _context.Products.Where(x => x.ProductId == productId)
+                                           .Select(x => new ReadProductModel
+                                           {
+                                               ProductId = x.ProductId,
+                                               Barcode = x.Barcode,
+                                               Description = x.Description,
+                                               Name = x.Name,
+                                               Price = $"{x.StandardPrice} PLN"
+                                           }).FirstOrDefault();
+            return product;
+        }
+
+        public IEnumerable<ReadProductModel> Read()
+        {
+            var products = _context.Products.Select(x => new ReadProductModel
+            {
+                ProductId = x.ProductId,
+                Barcode = x.Barcode,
+                Description = x.Description,
+                Name = x.Name,
+                Price = $"{x.StandardPrice} PLN"
+            }).ToList();
+
+            return products;
+        }
+
+        public void Delete(int productId)
+        {
+            var dbProduct = _context.Products.Where(x => x.ProductId == productId).FirstOrDefault();
+            _context.Products.Remove(dbProduct);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Rbyte/Src/Core/Rbyte.Application/Product/Read/ReadProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Rbyte.Application.Product.Read
+{
+    public class ReadProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public long Barcode { get; set; }
+    }
+}
