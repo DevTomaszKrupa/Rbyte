387c5fe31bee82641b51c9df029321b614503d58
Milosz P <miloszpasza@gmail.com>
2018-12-04 11:53:32 +0100

    Create CRUD and Controller for Tax

---
+++ Rbyte/Src/Core/Rbyte.Application/Tax/TaxService.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using Rbyte.Application.Tax.Create;
+using Rbyte.Application.Tax.Read;
+using Rbyte.Application.Tax.Update;
+using Rbyte.Domain.Entities;
+using Rbyte.Persistance;
+
+namespace Rbyte.Application.Tax
+{
+    public interface ITaxService
+    {
+        void Create(CreateTaxModel model);
+        ReadTaxModel Read(int taxId);
+        IEnumerable<ReadTaxModel> Read();
+        UpdateTaxModel GetForEdition(int taxId);
+        void Update(UpdateTaxModel model);
+        void Delete(int taxId);
+    }
+    public class TaxService : ITaxService
+    {
+        private readonly RbyteContext _context;
+        public TaxService(RbyteContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(CreateTaxModel model)
+        {
+            _context.Add(new DbTax
+            {
+                Value = model.Value
+            });
+            _context.SaveChanges();
+        }
+
+        public void Delete(int taxId)
+        {
+            var dbTax = _context.Taxes.First(x => x.TaxId == taxId);
+            _context.Taxes.Remove(dbTax);
+            _context.SaveChanges();
+        }
+
+        public UpdateTaxModel GetForEdition(int taxId)
+        {
+            var tax = _context.Taxes.Where(x => x.TaxId == taxId)
+                                    .Select(x => new UpdateTaxModel
+                                    {
+                                        TaxId = x.TaxId,
+                                        Value = x.Value
+                                    }).First();
+            return tax;
+        }
+
+        public ReadTaxModel Read(int taxId)
+        {
+            var tax = _context.Taxes.Where(x => x.TaxId == taxId)
+                                    .Select(x => new ReadTaxModel
+                                    {
+                                        TaxId = x.TaxId,
+                                        Value = x.Value
+                                    }).First();
+            return tax;
+        }
+
+        public IEnumerable<ReadTaxModel> Read()
+        {
+            var taxes = _context.Taxes.Select(x => new ReadTaxModel
+            {
+                TaxId = x.TaxId,
+                Value = x.Value
+            }).ToList();
+            return taxes;
+        }
+
+        public void Update(UpdateTaxModel model)
+        {
+            var dbTax = _context.Taxes.First(x => x.TaxId == model.TaxId);
+            dbTax.Value = model.Value;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Rbyte/Src/Presentation/Rbyte.Mvc/Controllers/TaxController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Rbyte.Application.Tax;
+using Rbyte.Application.Tax.Create;
+using Rbyte.Application.Tax.Update;
+
+namespace Rbyte.Mvc.Controllers
+{
+    public class TaxController : Controller
+    {
+        private readonly ITaxService _taxService;
+        public TaxController(ITaxService taxService)
+        {
+            _taxService = taxService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var list = _taxService.Read();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateTaxModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTaxModel model)
+        {
+            _taxService.Create(model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var tax = _taxService.Read(id);
+            return View(tax);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var tax = _taxService.GetForEdition(id);
+            return View(tax);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UpdateTaxModel model)
+        {
+            _taxService.Update(model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            _taxService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
