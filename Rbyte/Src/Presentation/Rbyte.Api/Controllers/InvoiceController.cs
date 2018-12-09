745e21c75e53d5bc839807ccd640202f733deb41
Tomasz Krupa <tomaszkrupa95@gmail.com>
2018-12-10 00:55:24 +0100

    Added invoice controller

---
+++ Rbyte/Src/Presentation/Rbyte.Api/Controllers/InvoiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Rbyte.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        // GET api/invoice
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "invoice1", "invoice2" };
+        }
+    }
+}
