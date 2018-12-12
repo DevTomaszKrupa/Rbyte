11bda6147fa64c5fb280c6e539ecee092aa811e5
Tomasz Krupa <tomaszkrupa95@gmail.com>
2018-12-13 00:36:35 +0100

    Added side nav item component

--- Rbyte/Src/Presentation/Rbyte.Angular/rbyte/src/app/layout/layout.module.ts
+++ Rbyte/Src/Presentation/Rbyte.Angular/rbyte/src/app/layout/layout.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { SideNavItemComponent } from './side-nav/side-nav-item/side-nav-item.component';
 
 @NgModule({
     imports: [
     ],
     declarations: [
-        SideNavComponent
+        SideNavComponent,
+        SideNavItemComponent
     ],
     providers: [
     ],
