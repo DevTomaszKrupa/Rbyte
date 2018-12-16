8e299c6368b81c6cba858eb7b30645ded80c941c
Adrian Krupa <adrian.krupa@o2.pl>
2018-12-16 18:21:43 +0100

    added MSSql connection

---
+++ Rbyte/Src/Infrastructure/Rbyte.Persistance/MSSql/MSSqlRbyteContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rbyte.Persistance.MySql
+{
+    public class MSSqlRbyteContext : RbyteContext
+    {
+        public MSSqlRbyteContext(DbContextOptions<MSSqlRbyteContext> options) : base(options)
+        {
+        }
+    }
+}
