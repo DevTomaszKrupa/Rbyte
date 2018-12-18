baca342e697220d219b7f56e9115136fb9ee24fb
mikolaj <mkolajkrupa00@o2.pl>
2018-12-18 23:02:22 +0100

    Add controller for tax, store and discount

---
+++ Rbyte/Src/Api/Rbyte.Api/Controllers/StoreController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rbyte.Api.Models.Store;
+using Rbyte.Application.Store;
+
+namespace Rbyte.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        public IStorehouseService _storehouseService { get; set; }
+
+        public StoreController(IStorehouseService storehouseService)
+        {
+            _storehouseService = storehouseService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var list = _storehouseService.Get();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var list = _storehouseService.Get(id);
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ApiStorehouse request)
+        {
+            _storehouseService.Create(request);
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody] ApiStorehouse request)
+        {
+            _storehouseService.Update(request);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _storehouseService.Delete(id);
+        }
+    }
+}
---
+++ Rbyte/Src/Api/Rbyte.Api/Controllers/TaxController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rbyte.Api.Models.Tax;
+using Rbyte.Application.Tax;
+
+namespace Rbyte.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxController : ControllerBase
+    {
+        public ITaxService _taxService { get; set; }
+
+        public TaxController(ITaxService taxService)
+        {
+            _taxService = taxService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var list = _taxService.Get();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var list = _taxService.Get(id);
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ApiTax request)
+        {
+            _taxService.Create(request);
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromBody] ApiTax request)
+        {
+            _taxService.Update(request);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _taxService.Delete(id);
+        }
+
+    }
+}
