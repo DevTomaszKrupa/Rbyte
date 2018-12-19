3409205ccddc3935771561ae6920cc8b8dcde9b6
mikolaj <mkolajkrupa00@o2.pl>
2018-12-19 18:09:56 +0100

    discountService - create discount test

---
+++ Rbyte/Tests/Rbyte.Tests/Application/Discount/DiscountServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Rbyte.Api.Models.Discount;
+using Rbyte.Application.Discount;
+using Rbyte.Persistance;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rbyte.Tests.Application.Discount
+{
+    public class DiscountServiceTests
+    {
+        [Fact]
+        public void Create_OneDiscount()
+        {
+            void Method(RbyteContext context)
+            {
+                // Arrange 
+                var discountService = new DiscountService(context);
+                var apiDiscount = new ApiDiscount
+                {
+                    Value = 7
+                };
+
+                //Act
+                discountService.Create(apiDiscount);
+
+                //Assert
+                var discounts = context.Discounts.ToArray();
+                discounts.Length.ShouldBe(1);
+                discounts[0].Value.ShouldBe(7);
+            }
+            RbyteContextActionInvoker.Invoke(Method);
+        }
+    }
+}
