27c4f2092976dc2f2de99ca3cb4d26c504aff9d0
mikolaj <mkolajkrupa00@o2.pl>
2018-12-19 18:06:06 +0100

    categoryService - create category test

---
+++ Rbyte/Tests/Rbyte.Tests/Application/Category/CategoryServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Rbyte.Api.Models.Category;
+using Rbyte.Application.Category;
+using Rbyte.Persistance;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rbyte.Tests.Application.Category
+{
+    public class CategoryServiceTests
+    {
+        [Fact]
+        public void Create_OneCategory()
+        {
+            void Method(RbyteContext context)
+            {
+                // Arrange
+                var categoryService = new CategoryService(context);
+                var apiCategory = new ApiCategory
+                {
+                    Description = "description",
+                    Name = "name",
+                };
+
+                // Act
+                categoryService.Create(apiCategory);
+
+                //Assert
+                var categories = context.Categories.ToArray();
+                categories.Length.ShouldBe(1);
+                categories[0].Name.ShouldBe("name");
+                categories[0].Description.ShouldBe("description");
+            }
+            RbyteContextActionInvoker.Invoke(Method);
+        }
+    }
+}
